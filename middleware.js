2b0c78cb63ddd700865dbfa2eac472bf02516003
micanagaoka <searchanddestroy7221@gmail.com>
2024-09-14 00:27:02 +0900

    null削除

--- middleware.js
+++ middleware.js
@@ -15,7 +15,7 @@ export default async function middleware(req) {
       const [providedUsername, providedPassword] = atob(encoded).split(':');
       
       if (providedUsername === username && providedPassword === password) {
-        return new Response(null, { status: 200 });
+        return new Response('Authenticated', { status: 200 });
       }
     }
   }
