6efca5031f1eda9ba7f761193fc7290093360b5d
micanagaoka <searchanddestroy7221@gmail.com>
2024-09-14 05:19:27 +0900

    @vercel/edgeインストール

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,18 +1,1 @@
-self.__BUILD_MANIFEST = {
-  "polyfillFiles": [
-    "static/chunks/polyfills.js"
-  ],
-  "devFiles": [],
-  "ampDevFiles": [],
-  "lowPriorityFiles": [],
-  "rootMainFiles": [],
-  "pages": {
-    "/_app": []
-  },
-  "ampFirstPages": []
-};
-self.__BUILD_MANIFEST.lowPriorityFiles = [
-"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
-,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
-
-];
+self.__BUILD_MANIFEST={polyfillFiles:["static/chunks/polyfills-78c92fac7aa8fdd8.js"],devFiles:[],ampDevFiles:[],lowPriorityFiles:[],rootMainFiles:["static/chunks/webpack-6b2f098d55ba02b2.js","static/chunks/fd9d1056-2821b0f0cabcd8bd.js","static/chunks/23-fcd997dff58d4f95.js","static/chunks/main-app-c0e8e5e5166da1ef.js"],pages:{"/_app":["static/chunks/webpack-6b2f098d55ba02b2.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-d206ebe66ad97e36.js","static/chunks/pages/_app-6a626577ffa902a4.js"],"/_error":["static/chunks/webpack-6b2f098d55ba02b2.js","static/chunks/framework-f66176bb897dc684.js","static/chunks/main-d206ebe66ad97e36.js","static/chunks/pages/_error-1be831200e60c5c0.js"]},ampFirstPages:[]},self.__BUILD_MANIFEST.lowPriorityFiles=["/static/"+process.env.__NEXT_BUILD_ID+"/_buildManifest.js",,"/static/"+process.env.__NEXT_BUILD_ID+"/_ssgManifest.js"];
