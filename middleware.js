02c3b7fdede11fa61ecf7819d99277a43cad22ca
micanagaoka <searchanddestroy7221@gmail.com>
2024-09-14 02:12:00 +0900

    nextへ変更

--- middleware.js
+++ middleware.js
@@ -1,26 +1,24 @@
+import { NextResponse } from 'next/server';
+
 export const config = {
   matcher: '/(.*)',
 };
 
-export default async function middleware(req) {
+export default function middleware(req) {
   const auth = req.headers.get('authorization');
   
-  const username = process.env.BASIC_AUTH_USERNAME;
-  const password = process.env.BASIC_AUTH_PASSWORD;
-
   if (auth) {
-    const [scheme, encoded] = auth.split(' ');
+    const [username, password] = atob(auth.split(' ')[1]).split(':');
     
-    if (scheme === 'Basic') {
-      const [providedUsername, providedPassword] = atob(encoded).split(':');
-      
-      if (providedUsername === username && providedPassword === password) {
-        return new Response('Authenticated', { status: 200 });
-      }
+    const expectedUsername = process.env.BASIC_AUTH_USERNAME;
+    const expectedPassword = process.env.BASIC_AUTH_PASSWORD;
+    
+    if (username === expectedUsername && password === expectedPassword) {
+      return NextResponse.next();
     }
   }
 
-  return new Response('Unauthorized', {
+  return new NextResponse('Unauthorized', {
     status: 401,
     headers: {
       'WWW-Authenticate': 'Basic realm="Secure Area"'
