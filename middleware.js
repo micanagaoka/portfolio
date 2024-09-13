2745f34bccbda70e03d38b2a92fb0b394ae175d7
micanagaoka <searchanddestroy7221@gmail.com>
2024-09-14 03:41:38 +0900

    importをedgeに

--- middleware.js
+++ middleware.js
@@ -1,27 +1,25 @@
-import { NextResponse } from 'next/server';
+import { next } from "@vercel/edge";
 
 export const config = {
   matcher: '/(.*)',
 };
 
-export default function middleware(req) {
-  const auth = req.headers.get('authorization');
-  
-  if (auth) {
-    const [username, password] = atob(auth.split(' ')[1]).split(':');
-    
-    const expectedUsername = process.env.BASIC_AUTH_USERNAME;
-    const expectedPassword = process.env.BASIC_AUTH_PASSWORD;
-    
-    if (username === expectedUsername && password === expectedPassword) {
-      return NextResponse.next();
+export default function middleware(request) {
+  const authorizationHeader = request.headers.get("authorization");
+
+  if (authorizationHeader) {
+    const basicAuth = authorizationHeader.split(" ")[1];
+    const [user, password] = atob(basicAuth).toString().split(":");
+
+    if (user === process.env.BASIC_AUTH_USER && password === process.env.BASIC_AUTH_PASSWORD) {
+      return next();
     }
   }
 
-  return new Response('Unauthorized', {
+  return new Response("Basic Auth required", {
     status: 401,
     headers: {
-      'WWW-Authenticate': 'Basic realm="Secure Area"'
-    }
+      "WWW-Authenticate": 'Basic realm="Secure Area"',
+    },
   });
 }
