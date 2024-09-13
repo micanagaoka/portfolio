51a84b3b3301a9a37a6f5e50f01944a51a052d32
micanagaoka <searchanddestroy7221@gmail.com>
2024-09-13 23:12:14 +0900

    middleware.jsのexport追加

--- middleware.js
+++ middleware.js
@@ -1,22 +1,22 @@
-export function middleware(request) {
-    const authHeader = request.headers.get('authorization');
-    if (authHeader) {
-        const base64Credentials = authHeader.split(' ')[1];
-        const credentials = atob(base64Credentials).split(':');
-        const username = credentials[0];
-        const password = credentials[1];
+export const config = {
+  matcher: '/(.*)'
+}
 
-        const validUser = process.env.BASIC_AUTH_USER;
-        const validPass = process.env.BASIC_AUTH_PASS;
-
-        if (username === validUser && password === validPass) {
-            return new Response(null, { status: 200 });
-        }
+export default function middleware(req) {
+  const auth = req.headers.get('authorization');
+  
+  if (auth) {
+    const [username, password] = atob(auth.split(' ')[1]).split(':');
+    
+    if (username === 'your-username' && password === 'your-password') {
+      return new Response(null, { status: 200 });
     }
-    return new Response('Unauthorized', {
-        status: 401,
-        headers: {
-            'WWW-Authenticate': 'Basic realm="Secure Area"',
-        },
-    });
+  }
+
+  return new Response('Unauthorized', {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="Secure Area"'
+    }
+  });
 }
